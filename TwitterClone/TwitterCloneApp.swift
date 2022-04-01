1a2c3bd502ccade2193f78eec003ba3b44b24a2f
Yahia <erifaj@outlook.com>
2022-04-01 19:39:57 +0300

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Mohamad Yahia on 1.4.2022.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
