e5093bd8e17aea5508457218a950e33eba751ad7
Yahia <yahimoh@icloud.com>
2022-04-07 22:03:31 +0300

    Explore view UI started

---
+++ TwitterClone/Core/Explore/ViewModels/ExploreView.swift
@@ -0,0 +1,35 @@
+//
+//  ExploreView.swift
+//  TwitterClone
+//
+//  Created by Mohamad Yahia on 3.4.2022.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                ScrollView {
+                    LazyVStack {
+                        ForEach(0 ... 25, id: \.self) { _ in
+                            NavigationLink {
+                                ProfileView()
+                            } label: {
+                                UserRowView()
+                            }
+                        }
+                    }
+                }
+            }.navigationTitle("Explore")
+                .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct ExploreView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExploreView()
+    }
+}
